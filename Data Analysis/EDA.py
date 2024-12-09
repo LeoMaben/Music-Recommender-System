3fd002bbe535e42ed89daf84fd52cf8e4c8b1055
Leonardo <leomaben@hotmail.com>
2024-12-09 19:27:59 +0200

    Initial commit of EDA.py

---
+++ Data Analysis/EDA.py
@@ -0,0 +1,19 @@
+"""
+@Author: Leonardo Maben
+@Date: 1/12/24
+
+"""
+import numpy
+from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
+
+
+
+
+
+def main():
+    print("Test")
+
+
+if __name__ == '__main__':
+    main()
