4be47b51356bf70d17adc15c125d86708f27610b
Leonardo <leomaben@hotmail.com>
2024-12-22 15:08:16 +0400

    EDA - Top playlist along with PCA of the data

--- Data Analysis/EDA.py
+++ Data Analysis/EDA.py
@@ -33,10 +33,10 @@ def aggregate_data(folder_path):
 
 
 def top_songs(dataframe):
-    '''
+    """
     Plot the top 10 songs
     :param dataframe: Dataframe containing the playlist metadata
-    '''
+    """
     song_count = dataframe['Track Name'].value_counts().head(10)
     sns.barplot(x=song_count.values, y=song_count.index)
     plt.title('The top 10 most popular songs in playlists are: ')
@@ -44,24 +44,66 @@ def top_songs(dataframe):
 
 
 def top_artists(dataframe):
-    '''
+    """
     Plots the top 10 artists
     :param dataframe: Dataframe containing the playlist metadata
-    '''
+    """
     artist_count = dataframe['Artist Name'].value_counts().head(10)
     sns.barplot(x=artist_count.values, y=artist_count.index)
     plt.title('The top 10 most popular artist in the playlists are:')
     plt.show()
 
+
+def top_playlist(dataframe):
+    """
+    Displays the playlists with the most number of songs
+    :param dataframe: Dataframe containing the playlist metadata
+    """
+
+    playlist_count = dataframe['Playlist ID'].value_counts().head(10)
+    sns.barplot(x=playlist_count.values, y=playlist_count.index, errorbar=None)
+    plt.title('The top 10 biggest playlists are: ')
+    plt.show()
+
+
+def apply_PCA(dataframe):
+    """
+    Applies a PCA Analysis of the metadata invovling the playlist
+
+    :param dataframe: Dataframe containing the playlist metadata
+    """
+    interaction_matrix = dataframe.pivot_table(index='Playlist ID', columns='Track Url', aggfunc='size', fill_value=0)
+
+    # Convert to a NumPy array for PCA
+    interaction_array = interaction_matrix.values
+    print("Interaction Matrix Shape:", interaction_array.shape)
+
+    pca = PCA(n_components=2)
+    reduced_data = pca.fit_transform(interaction_array)
+
+    # Explained variance ratio
+    print("Explained Variance Ratio:", pca.explained_variance_ratio_)
+
+    # Create a 2D scatter plot
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(reduced_data[:, 0], reduced_data[:, 1], alpha=0.7, s=10, color='blue')
+    plt.title("2D PCA Visualization of Playlists")
+    plt.xlabel("Principal Component 1")
+    plt.ylabel("Principal Component 2")
+    plt.grid()
+    plt.show()
+
+
 def main():
     # folder_path = '../metadata/'
     playlist_df = pd.read_csv('aggregated_data.csv')
 
-    #top_songs(playlist_df)
-    top_artists(playlist_df)
-
+    # top_songs(playlist_df)
+    # top_artists(playlist_df)
     # aggregate_data(folder_path)
-
+    # print(playlist_df.count())
+    apply_PCA(playlist_df)
 
 if __name__ == '__main__':
     main()
